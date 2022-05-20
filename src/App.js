fd65a9e0777154a07781a4163954c1ba0782def2
aryamallik <ac755041@gmail.com>
2022-05-20 16:34:25 +0530

    push

--- src/App.js
+++ src/App.js
@@ -4,33 +4,34 @@ import Login from "./components/Login";
 import PageNotFound from "./components/PageNotFound";
 import Profile from "./components/Profile";
 import Signup from "./components/Signup";
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  <>
+  return (
+    <>
+      <Switch>
+        <Route path="/feed">
+          <Feed />
+        </Route>
 
-  <Switch>
-    <Route path="/feed">
-    <Feed />
-    </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
 
-    <Route path="/feed">
-    <Login />
-    </Route>
-    
-    <Route path="/feed">
-    <Signup />
-    </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
 
-    <Route path="/feed">
-    <Profile />
-    </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
 
-    <Route path="/feed">
-    <PageNotFound />
-    </Route>
-  </Switch>
-  </>;
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
