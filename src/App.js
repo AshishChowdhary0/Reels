984a7ec5f65830d81f637a1f448c45db722e646e
aryamallik <ac755041@gmail.com>
2022-05-20 15:33:13 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import Feed from "./components/Feed";
+import Login from "./components/Login";
+import PageNotFound from "./components/PageNotFound";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import { Switch, Route } from "react-router-dom"
 
 function App() {
-  return <div> Reels Project </div>;
+  <>
+
+  <Switch>
+    <Route path="/feed">
+    <Feed />
+    </Route>
+
+    <Route path="/feed">
+    <Login />
+    </Route>
+    
+    <Route path="/feed">
+    <Signup />
+    </Route>
+
+    <Route path="/feed">
+    <Profile />
+    </Route>
+
+    <Route path="/feed">
+    <PageNotFound />
+    </Route>
+  </Switch>
+  </>;
 }
 
 export default App;
